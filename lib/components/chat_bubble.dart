7783eef9781ae4eb787f244d7fb771e5ca8f62d0
therealcoder241 <hackerlbv241@gmail.com>
2025-01-14 21:14:52 +0100

    latest veille

---
+++ lib/components/chat_bubble.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final String message;
+  final bool isCurrentUser;
+  const ChatBubble({
+    super.key,
+    required this.message,
+    required this.isCurrentUser,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
